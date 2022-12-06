1d84fda9e789c0339da04e6db780aa33014d8d46
dbguettler <69404804+dbguettler@users.noreply.github.com>
2022-12-06 00:20:10 -0500

    Convert day2 to TypeScript

--- day2/main.ts
+++ day2/main.ts
@@ -1,9 +1,18 @@
-const utils = require("../shared/utils");
+import { getLines } from "../shared/utils";
 
 const startTime = Date.now();
-const lines = utils.getLines(process.argv);
+const lines = getLines(process.argv);
 
-const SCORES = {
+interface ScoreOuter {
+  [key: string]: ScoreInner;
+}
+
+interface ScoreInner {
+  [key: string]: number;
+}
+
+// Maps SCORES[outer][inner] to a score, where outer is the opponent's move and inner is your move
+const SCORES: ScoreOuter = {
   A: {
     X: 1 + 3,
     Y: 2 + 6,
@@ -21,7 +30,16 @@ const SCORES = {
   },
 };
 
-const MOVES = {
+interface MoveOuter {
+  [key: string]: MoveInner;
+}
+
+interface MoveInner {
+  [key: string]: string;
+}
+
+// Maps MOVES[outer][inner] to your move letter, where outer is the opponent's move and inner is the desired game outcome
+const MOVES: MoveOuter = {
   A: {
     X: "Z",
     Y: "X",
@@ -42,8 +60,8 @@ const MOVES = {
 let two = 0;
 let one = 0;
 for (const line of lines) {
-  const opponent = line.at(0);
-  const sym = line.at(2);
+  const opponent = line[0];
+  const sym = line[2];
   one += SCORES[opponent][sym];
   const self = MOVES[opponent][sym];
   two += SCORES[opponent][self];
